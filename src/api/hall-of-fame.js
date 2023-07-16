1bc1eb27e4b7c59a36d669c1e81fbbe7e66e5572
Alexey Aleynikov <alexey.aleynikov@telekom.com>
2023-07-16 13:12:42 +0300

    added vercel

--- src/api/hall-of-fame.js
+++ src/api/hall-of-fame.js
@@ -1,20 +1,28 @@
+// hall-of-fame.js
 const express = require('express');
+const path = require('path');
 const { google } = require('googleapis');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 const app = express();
+// Разрешение запросов CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3002'); // Укажите URL вашего клиентского приложения
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '..', 'client/build')));
 
 // API endpoint for hall of fame data
 app.get('/hall-of-fame', async (req, res) => {
-  try {
-    // Ваш код для обработки данных таблицы Google Sheets
-    // ...
+  // Ваш код обработки запроса
+});
 
-    res.json(usersData);
-  } catch (error) {
-    console.error('Error fetching hall of fame data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+// Handle other routes and serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client/build', 'index.html'));
 });
 
 module.exports = app;
