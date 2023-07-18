e5460acdb4c1482d8fe5e00cf7ce815488081003
Alexey Aleynikov <alexey.aleynikov@telekom.com>
2023-07-18 17:03:55 +0300

    added gallery

--- src/imported_js/ImagePreviewModal.js
+++ src/imported_js/ImagePreviewModal.js
@@ -4,7 +4,7 @@ import React from 'react';
 const ImagePreviewModal = ({ imageUrl, onClose }) => {
   return (
     <div className="image-modal" onClick={onClose}>
-      <img src={imageUrl} alt="Image Preview" style={{ maxWidth: '50vw', height: 'auto' }}/>
+      <img src={imageUrl} alt="Image Preview" style={{ maxWidth: '66vw', height: 'auto' }}/>
 
     </div>
   );
