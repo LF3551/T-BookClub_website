832a37b30c14d02484d830cd90ed02a4f13be73f
Alexey Aleynikov <alexey.aleynikov@telekom.com>
2023-07-20 08:55:35 +0300

    added уьм

--- src/imported_js/readEnvFile.js
+++ src/imported_js/readEnvFile.js
@@ -1,10 +1,7 @@
 
 const readEnvFile = () => {
     try {
-      const envData =  String("https://t-book-club-server-lf3551.vercel.app/hall-of-fame");
-    //   const envData = process.env.REACT_APP_MY_VARIABLE
-      console.log(envData); // Выводим содержимое строки в консоль
-      return envData;
+      return process.env.REACT_APP_API_URL;
     } catch (error) {
       console.error('Error reading env.json:', error);
       return null;
