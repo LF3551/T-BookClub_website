0fb77b97229f830260c5c79f9433dc31ab8cf03d
Alexey Aleynikov <alexey.aleynikov@telekom.com>
2023-08-18 16:18:41 +0300

    added flags

--- src/FutureEventsPage.js
+++ src/FutureEventsPage.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './css/FutureEventsPage.css';
 import readEnvFile2 from './imported_js/readEnvFile2';
-
+import enImage from './images/en.png';
+import ruImage from './images/ru.png';
 
 function FutureEventsPage() {
     const [events, setEvents] = useState([]);
@@ -13,7 +14,8 @@ function FutureEventsPage() {
         try {
             const serverurl2 = readEnvFile2();
             console.log(serverurl2)
-            const response = await fetch(serverurl2);
+            // put this serverurl2
+            const response = await fetch("https://t-book-club-server-lf3551.vercel.app/future_events");
          
             const data = await response.json();
             console.log(data)
@@ -22,37 +24,67 @@ function FutureEventsPage() {
           console.error('Error fetching future events:', error);
         }
       };
+      
     const currentDate = new Date();
-    const sortedEvents = [...events].sort((a, b) => {
-        const dateA = new Date(a.Date);
-        const dateB = new Date(b.Date);
-        return dateB - dateA;  // Это упорядочит события по убыванию даты.
-    });
-    const pastEvents = sortedEvents.filter(event => new Date(event.Date) < currentDate);
-    const futureEvents = sortedEvents.filter(event => new Date(event.Date) >= currentDate);
-
+    const stringToDate = (dateString) => {
+        const [day, month, year] = dateString.split('.');
+        return new Date(year, month - 1, day);
+    }
     
+    // Сначала сортировка всех событий
+    const sortedEvents = [...events].sort((a, b) => stringToDate(a.Date) - stringToDate(b.Date));
+    
+    // Затем фильтрация для будущих событий
+    const futureEvents = sortedEvents.filter(event => stringToDate(event.Date) >= currentDate);
+    
+    const pastEvents = sortedEvents.filter(event => stringToDate(event.Date) < currentDate).sort((a, b) => stringToDate(b.Date) - stringToDate(a.Date));
+    const languageToImage = {
+        en: enImage,
+        ru: ruImage
+      };
       return (
         <div className="future-events-page">
           <header className="app-header">
-            <h1 className="welcome-text" style={{ color: 'white' }}>
-              <span className="club-name">Future Events</span>
-            </h1>
+            <h1 className="welcome-text" style={{ color: 'white' }}><span className="club-name">Future Events</span></h1>
           </header>
-          <div className="events-content" style={{ color: 'white' }}>
-            {sortedEvents.map((event, index) => (
+          <div className="events-content" style={{ color: 'white'  }}>
+            {futureEvents.map((event, index) => (
                 <div key={index} className="event-item" style={{ color: 'white' }}>
-                    <h2 style={{ color: 'white' }}>{event.Date}</h2>
-                    <ul style={{ color: 'white' }}>
-                        {event.Events.map((e, idx) => (
-                            <li key={idx} style={{ color: 'white' }}>
-                                <strong style={{ color: 'white' }}>{e.Title}</strong> - {e.Discussion} ({e.Language})
-                            </li>
-                        ))}
-                    </ul>
+                    <h2 style={{ color: 'white', textAlign: 'center', width: '100%' }}>{event.Date}</h2>
+                    <table className="event-table">
+                        <tbody>
+                            {event.Events.map((e, idx) => (
+                                <tr key={idx}>
+                                    <td style={{ color: 'white', verticalAlign: 'top', textAlign: 'center' }}><strong>{e.Title}</strong></td>
+                                    <td style={{ color: 'white' }}>{e.Discussion.split(';').map((discussionItem, dIdx) => (<div key={dIdx} style={{ marginBottom: '3mm' }}>{discussionItem.trim()}</div>))}</td>
+                                    <td style={{ textAlign: 'center', verticalAlign: 'top', textAlign: 'center' }}><img src={languageToImage[e.Language]} alt={e.Language} style={{ width: '35%', backgroundColor: 'transparent' }} /></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
             ))}
         </div>
+        <Link to="/" className="future-events-back-button">Back</Link>
+        <h2 className="welcome-text" style={{ color: 'white' , marginTop: '2cm'}}><span className="club-name">Past Events</span></h2>
+        <div className="events-content" style={{ color: 'white' }}>
+    {pastEvents.map((event, index) => (
+        <div key={index} className="event-item" style={{ color: 'white' }}>
+            <h2 style={{ color: 'white', textAlign: 'center', width: '100%' }}>{event.Date}</h2>
+            <table className="event-table">
+                <tbody>
+                    {event.Events.map((e, idx) => (
+                        <tr key={idx}>
+                            <td style={{ color: 'white', verticalAlign: 'top', textAlign: 'center' }}><strong>{e.Title}</strong></td>
+                            <td style={{ color: 'white' }}>{e.Discussion.split(';').map((discussionItem, dIdx) => (<div key={dIdx} style={{ marginBottom: '3mm' }}>{discussionItem.trim()}</div>))}</td>
+                            <td style={{ textAlign: 'center', verticalAlign: 'top', textAlign: 'center' }}><img src={languageToImage[e.Language]} alt={e.Language} style={{ width: '35%', backgroundColor: 'transparent' }} /></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    ))}
+</div>
           <Link to="/" className="future-events-back-button">Back</Link>
         </div>
       );
